e0072a3dc5a203d49b87d6a045c4cd7349f83f18
joshp <joshpoole04@gmail.com>
2026-05-09 13:08:59 +0100

    20260509 - os enviroment defult change for pi compatability

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
+import os
 import sys
+os.environ.setdefault("QT_QPA_PLATFORM", "xcb")
+
 from PyQt5.QtWidgets import QApplication, QStackedWidget
 from PyQt5.QtCore import QProcess
 from home_screen import HomeScreen
